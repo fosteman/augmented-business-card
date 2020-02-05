b268716830731c66455be5efe8f9463c5d499d61
Timofei Shchepkin <dgrz5.0@gmail.com>
2020-02-05 10:54:05 -0500

    Multiple Image Detection targets

--- js/BusinessCard.js
+++ js/BusinessCard.js
@@ -1,5 +1,3 @@
-'use strict'
-
 import React, {Component} from 'react'
 
 import {StyleSheet} from 'react-native'
@@ -19,96 +17,25 @@ import {
 } from 'react-viro'
 
 export default class BusinessCard extends Component {
-
+  _onInitialized = (state, reason) => {
+    if (state == ViroConstants.TRACKING_NORMAL) {
+      isTracking: true
+    } else if (state == ViroConstants.TRACKING_NONE) {
+      isTracking: false
+    }
+  }
   state = {
     isTracking: false,
     initialized: false,
     runAnimation: false,
   }
 
-  getNoTrackingUI() {
-    const {isTracking, initialized} = this.state
-    return (
-      <ViroText text={
-        initialized ? 'Initializing AR...'
-          : 'No Tracking'
-      }/>
-    )
-  }
-
+  getNoTrackingUI = () => <ViroText text={this.state.initialized ? 'Initializing AR...' : 'No Tracking'}/>
 
   getARScene() {
     return (
       <ViroNode>
-        <ViroARImageMarker target={'businessCard'}
-                           onAnchorFound={() => this.setState({runAnimation: true})}
-        >
-          <ViroNode key="card">
-            <ViroNode
-              opacity={0} position={[0, -0.02, 0]}
-              animation={{
-                name: 'animateImage',
-                run: this.state.runAnimation,
-              }}
-            >
-              <ViroFlexView
-                rotation={[-90, 0, 0]}
-                height={0.03}
-                width={0.05}
-                style={styles.card}
-              >
-                <ViroFlexView
-                  style={styles.cardWrapper}
-                >
-                  <ViroImage
-                    height={0.015}
-                    width={0.015}
-                    style={styles.image}
-                    source={require('./res/Maverick.png')}
-                  />
-                  <ViroText
-                    textClipMode="None"
-                    text="Bradley Heath"
-                    scale={[.015, .015, .015]}
-                    style={styles.textStyle}
-                  />
-                </ViroFlexView>
-                <ViroFlexView
-                  onTouch={() => alert('twitter')}
-                  style={styles.subText}
-                >
-                  <ViroText
-                    width={0.01}
-                    height={0.01}
-                    textAlign="left"
-                    textClipMode="None"
-                    text="@extouring"
-                    scale={[.01, .01, .01]}
-                    style={styles.textStyle}
-                  />
-                  <ViroAnimatedImage
-                    height={0.01}
-                    width={0.01}
-                    loop={true}
-                    source={require('./res/tweet.gif')}
-                  />
-                </ViroFlexView>
-              </ViroFlexView>
-            </ViroNode>
-            <ViroNode opacity={0} position={[0, 0, 0]}
-                      animation={{
-                        name: 'animateWeb',
-                        run: this.state.runAnimation,
-                      }}
-            >
-              <ViroText text="extouring.travel"
-                        rotation={[-90, 0, 0]}
-                        scale={[.01, .01, .01]}
-                        style={styles.textStyle}
-              />
-            </ViroNode>
-          </ViroNode>
-        </ViroARImageMarker>
+        {targets.map(target => this.mark(target))}
       </ViroNode>
     )
   }
@@ -119,14 +46,6 @@ export default class BusinessCard extends Component {
         {this.state.isTracking ? this.getNoTrackingUI() : this.getARScene()}
       </ViroARScene>
     )
-  }
-
-  _onInitialized = (state, reason) => {
-    if (state == ViroConstants.TRACKING_NORMAL) {
-      isTracking: true
-    } else if (state == ViroConstants.TRACKING_NONE) {
-      isTracking: false
-    }
   }
 }
 
@@ -156,12 +75,113 @@ var styles = StyleSheet.create({
                                    flex: .5,
                                  },
                                })
-
+function mark(target) {
+  return (
+    <ViroARImageMarker target={target.name}
+                       onAnchorFound={() => this.setState({runAnimation: true})}
+    >
+      <ViroNode key="card">
+        <ViroNode
+          opacity={0} position={[0, -0.02, 0]}
+          animation={{
+            name: 'animateImage',
+            run: this.state.runAnimation,
+          }}
+        >
+          <ViroFlexView
+            rotation={[-90, 0, 0]}
+            height={0.03}
+            width={0.05}
+            style={styles.card}
+          >
+            <ViroFlexView
+              style={styles.cardWrapper}
+            >
+              <ViroImage
+                height={0.015}
+                width={0.015}
+                style={styles.image}
+                source={require(target.source)}
+              />
+              <ViroText
+                textClipMode="None"
+                text="target.title"
+                scale={[.015, .015, .015]}
+                style={styles.textStyle}
+              />
+            </ViroFlexView>
+            <ViroFlexView
+              onTouch={() => alert('twitter')}
+              style={styles.subText}
+            >
+              <ViroText
+                width={0.01}
+                height={0.01}
+                textAlign="left"
+                textClipMode="None"
+                text={target.connectionString}
+                scale={[.01, .01, .01]}
+                style={styles.textStyle}
+              />
+              <ViroAnimatedImage
+                height={0.01}
+                width={0.01}
+                loop={true}
+                source={require('./res/tweet.gif')}
+              />
+            </ViroFlexView>
+          </ViroFlexView>
+        </ViroNode>
+        <ViroNode opacity={0} position={[0, 0, 0]}
+                  animation={{
+                    name: 'animateWeb',
+                    run: this.state.runAnimation,
+                  }}
+        >
+          <ViroText text="extouring.travel"
+                    rotation={[-90, 0, 0]}
+                    scale={[.01, .01, .01]}
+                    style={styles.textStyle}
+          />
+        </ViroNode>
+      </ViroNode>
+    </ViroARImageMarker>
+  )
+}
+const targets = [
+  Target('extouring', './res/extouring-target.JPG', 'Call me maybe XX - BigB', 0.005, 'Bradley Heath'),
+  Target('In a heartbeat', './res/heart.png', 'Love Peace & Harmony', 0.5588, "Salwa Abdelsamie"),
+  Target('Fire', './res/Fire.png', "One can appreciate Fire in case he once froze to bones", 0.4572, "Saeideh B.T."),
+]
+class Target {
+  constructor(name, source, title, physicalWidth, connectionString) {
+    this.name = name
+    this.source = source
+    this.title = title
+    this.physicalWidth = physicalWidth
+    this.connectionString = connectionString
+  }
+  name: String //target's defined name
+  title: String
+  connectionString: String // twitter username
+  // avatar: String // img filename
+  physicalWidth: Number
+}
 ViroARTrackingTargets.createTargets({
-                                      'businessCard': {
+                                      'In a heartbeat': {
+                                        source: require('./res/heart.png'),
+                                        orientation: 'Up',
+                                        physicalWidth: 0.5588,
+                                      },
+                                      'Fire': {
+                                        source: require('./res/Fire.png'),
+                                        orientation: 'Up',
+                                        physicalWidth: 0.4572,
+                                      },
+                                      'extouring': {
                                         source: require('./res/extouring-target.JPG'),
                                         orientation: 'Up',
-                                        physicalWidth: 0.05,
+                                        physicalWidth: 0.005,
                                       },
                                     })
 
@@ -177,11 +197,11 @@ ViroMaterials.createMaterials({
 ViroAnimations.registerAnimations({
                                     animateImage: {
                                       properties: {
-                                        positionX: 0.05,
+                                        positionX: 0.08,
                                         opacity: 1.0,
                                       },
                                       easing: 'Bounce',
-                                      duration: 500,
+                                      duration: 800,
                                     },
                                     animateWeb: {
                                       properties: {
@@ -189,7 +209,7 @@ ViroAnimations.registerAnimations({
                                         opacity: 1.0,
                                       },
                                       easing: 'Bounce',
-                                      duration: 500,
+                                      duration: 800,
                                     },
                                   })
 
